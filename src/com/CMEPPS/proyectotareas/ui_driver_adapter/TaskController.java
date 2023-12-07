8e7b30965939f643f48801246c91ed5fa3a65e1c
Ivan <Ivan@DESKTOP-6A168VS>
2023-12-07 14:50:20 +0100

    JSP Creados

--- src/com/CMEPPS/proyectotareas/ui_driver_adapter/TaskController.java
+++ src/com/CMEPPS/proyectotareas/ui_driver_adapter/TaskController.java
@@ -2,6 +2,8 @@ package com.CMEPPS.proyectotareas.ui_driver_adapter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.CMEPPS.proyectotareas.core.domain.TaskDoesNotExistException;
 import com.CMEPPS.proyectotareas.core.driver_ports.TaskService;
 
-@RestController
+@Controller
 public class TaskController {
     @Autowired
     private TaskService taskService;
@@ -23,4 +25,6 @@ public class TaskController {
             return ResponseEntity.badRequest().build();
         }
     }
+    
+   
 }

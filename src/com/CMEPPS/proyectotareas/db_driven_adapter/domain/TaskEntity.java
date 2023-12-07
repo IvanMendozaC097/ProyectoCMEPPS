8ecc2cae7296f4f473d538d33a38401d219c8594
Jesús <Jess@DESKTOP-LS8LHC6>
2023-12-07 15:57:58 +0100

    reparado las interfaces

--- src/com/CMEPPS/proyectotareas/db_driven_adapter/domain/TaskEntity.java
+++ src/com/CMEPPS/proyectotareas/db_driven_adapter/domain/TaskEntity.java
@@ -5,12 +5,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import com.CMEPPS.proyectotareas.core.domain.Task;
-import com.CMEPPS.proyectotareas.core.domain.User;
 
 
 @Entity
+@Table(name = "task")
 public class TaskEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
--- src/com/CMEPPS/proyectotareas/ui_driver_adapter/WelcomeController.java
+++ src/com/CMEPPS/proyectotareas/ui_driver_adapter/WelcomeController.java
@@ -3,10 +3,12 @@ package com.CMEPPS.proyectotareas.ui_driver_adapter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class WelcomeController {
-    @GetMapping("/")
+	@RequestMapping(value = "/", method = RequestMethod.GET)
     public String showWelcomePage(ModelMap model) {
         return "welcome";
     }
